e1f1dd6a0df09fa23dd3b615c377b119dea538bb
Ulises <ulisesapaza.280@gmail.com>
2024-12-26 01:57:49 -0300

    refactor: update icon display to use links and simplify icon search functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,16 @@
-"use client"
+import Link from "next/link"
+import { AvailableIcons } from "@/types/IAvailableIcons"
+export default async function Page() {
+    const icons = Object.keys(AvailableIcons)
 
-// src/components/IconSearch.js
-import React, { useState } from 'react';
-import * as Icons from 'react-icons/fa'; // Importa todos los íconos de Font Awesome como ejemplo
-
-export default function Page() {
-    const [searchTerm, setSearchTerm] = useState<string>('');
-
-    // Filtrar íconos según el término de búsqueda
-    const filteredIcons = Object.keys(Icons).filter(iconName =>
-        iconName.toLowerCase().includes(searchTerm.toLowerCase())
-    );
-
-    return (
-        <div>
-            <input
-                className='w-full p-2 border border-gray-300 rounded-md'
-                type="text"
-                placeholder="Buscar íconos..."
-                value={searchTerm}
-                onChange={(e) => setSearchTerm(e.target.value)}
-            />
-            <div className='grid grid-cols-4 gap-4'>
-                {filteredIcons.map(iconName => {
-                    const IconComponent = Icons[iconName as keyof typeof Icons];
-                    return (
-                        <div key={iconName} className='flex flex-col items-center justify-center'>
-                            <div className='w-10 h-10 bg-gray-200 rounded-md'>
-                                <IconComponent className='w-full h-full' color='black' />
-                            </div>
-                            <span>{iconName}</span>
-                        </div>
-                    );
-                })}
-            </div>
-        </div>
-    );
-};
+    return <div>
+        <h1 className="text-2xl font-bold">Available Icons</h1>
+        <ul className="flex flex-wrap gap-2">
+            {icons.map((icon) => (
+                <li key={icon} className="bg-gray-200 rounded-md">
+                    <Link className="w-full h-full p-6" href={`/${icon}`}>{icon}</Link>
+                </li>
+            ))}
+        </ul>
+    </div>
+}
